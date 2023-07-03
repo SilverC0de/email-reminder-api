f5a9c092bef8f126b361fc265329dc59c4b10051
jakusha <aaronebube123@gmail.com>
2023-07-03 23:31:02 +0100

    chore: eslint, prettier,trebble setup

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const { useTreblle } = require("treblle");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+const { TREBLE_PROJECT_ID, TREBLE_API_KEY } = process.env;
+console.log(TREBLE_API_KEY, TREBLE_PROJECT_ID);
+useTreblle(app, {
+  apiKey: TREBLE_API_KEY,
+  projectId: TREBLE_PROJECT_ID,
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "hello world" });
